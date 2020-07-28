b52423c6e96305a2e3bc721f02c0fd7524b357f7
jinjian <250354848@qq.com>
2020-07-28 21:46:49 +0800

    提交 爱的excel

--- src/test/java/com/test/demo/nospring/HandlerExcel.java
+++ src/test/java/com/test/demo/nospring/HandlerExcel.java
@@ -1,5 +1,7 @@
 package com.test.demo.nospring;
 
+import lombok.Data;
+import lombok.Getter;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
@@ -54,32 +56,35 @@ public class HandlerExcel {
             write.setSheetName(n, sheet.getSheetName());
             int count = 0;
             int rowNum = 0;
-            BigDecimal bigDecimal = BigDecimal.ZERO;
-            for (int i = 4; i < rows; i++) {//从第四列开始
+
+
+            BigDecimal bigDecimal = new BigDecimal("0");//数量
+
+            for (int i = 4; i <= rows; i++) {//从第四列开始
                 Row row = sheet.getRow(i);
                 System.out.println("处理的sheet是：" + sheet.getSheetName() + ";");
                 rowNum = i;//处理的行数
                 if (i < rows) {
                     if (Objects.nonNull(row)) {
                         System.out.println("处理行数：" + i);
-                        count = handlerRow(write, row, writeSheet, count, rowNum, bigDecimal);
-                        System.out.println("");
+                        ReturnMessage returnMessage = handlerRow(write, row, writeSheet, count, rowNum, bigDecimal);
+                        count=returnMessage.getCount();
+                        bigDecimal=returnMessage.getBigDecimal();
                     } else {
                         continue;
                     }
 
+                } else {
+                    //等于
+                    Row row1 = write.getSheetAt(n).createRow(count + 1);
+                    Cell cell3 = row1.createCell(3);
+                    cell3.setCellType(CellType.STRING);
+                    cell3.setCellValue(count);
+
+                    Cell cell7 = row1.createCell(7);
+                    cell7.setCellType(CellType.STRING);
+                    cell7.setCellValue(bigDecimal.toPlainString());
                 }
-//                else {
-//                    //等于
-//                    Row row1 = write.getSheetAt(n).createRow(rowNum);
-//                    Cell cell3 = row1.createCell(3);
-//                    cell3.setCellType(CellType.STRING);
-//                    cell3.setCellValue(count);
-//
-//                    Cell cell7 = row1.createCell(7);
-//                    cell7.setCellType(CellType.STRING);
-//                    cell7.setCellValue(bigDecimal.toPlainString());
-//                }
             }
             System.out.println("生成总列数" + rowNum);
 
@@ -101,30 +106,39 @@ public class HandlerExcel {
      * @param workbook 模板数据
      * @param row      数据源 row
      */
-    static int handlerRow(Workbook workbook, Row row, Sheet sheet, int count, int rowNum, BigDecimal bigDecimal) {
+    static ReturnMessage handlerRow(Workbook workbook, Row row, Sheet sheet, int count, int rowNum, BigDecimal bigDecimal) {
+        ReturnMessage returnMessage=new ReturnMessage();
         row.getCell(row.getLastCellNum() - 1).setCellType(CellType.STRING);
         String[] codes = row.getCell(row.getLastCellNum() - 1).getStringCellValue().split(",");
         String[] sizes = row.getCell(9).getStringCellValue().split("-");
         for (int j = 0; j < codes.length; j++) {
             //创建单条记录
-            Row row1 = sheet.createRow(count+2);//从第二列开始写
+            Row row1 = sheet.createRow(count + 2);//从第二列开始写
             Cell temp = row1.createCell(0);
-            temp.setCellValue(1+count++);
+            temp.setCellValue(1 + count++);
 
 
             serCell(row1, row, 1, 1);
             serCell(row1, row, 2, 2);
             Cell cell3 = row1.createCell(3);
-            cell3.setCellValue("1");
+
+            row.getCell(3).setCellType(CellType.STRING);
+            String counts = row.getCell(3).getStringCellValue();
+            int number = Integer.valueOf(counts) / codes.length;
+            cell3.setCellValue(number);//数量
 
 
             serCell(row1, row, 4, 4);//单价
             serCell(row1, row, 6, 5);//折扣
-            serCell(row1, row, 7, 6);//折后单价
-            serCell(row1, row, 8, 7);//折后金额
-            // 总金额
-//                row.getCell(7).setCellType(CellType.STRING);
-//            bigDecimal = bigDecimal.add(new BigDecimal(row.getCell(7).getStringCellValue()));//
+            String singleMoney = serCell(row1, row, 7, 6);//折后单价
+            Cell cell4 = row1.createCell(7);
+
+            row.getCell(8).setCellType(CellType.STRING);
+            BigDecimal bigDecimal1 = new BigDecimal(singleMoney).multiply(new BigDecimal(number));
+            cell4.setCellValue(bigDecimal1.toPlainString());//折扣价等于折扣价乘以数量
+
+
+            bigDecimal = bigDecimal.add(bigDecimal1);//计算折后价总金额
 
             Cell cell8 = row1.createCell(8);//尺寸
             cell8.setCellValue(sizes[j]);//
@@ -133,7 +147,9 @@ public class HandlerExcel {
             rowNum = rowNum + 1;
 
         }
-        return count;
+        returnMessage.setBigDecimal(bigDecimal);
+        returnMessage.setCount(count);
+        return returnMessage;
     }
 
     /**
@@ -142,12 +158,12 @@ public class HandlerExcel {
      * @param num     原始cell 位置
      * @param cellNum 需要创建的位置
      */
-    static void serCell(Row row1, Row row, int num, int cellNum) {
+    static String serCell(Row row1, Row row, int num, int cellNum) {
         Cell cell4 = row1.createCell(cellNum);
 
         row.getCell(num).setCellType(CellType.STRING);
         cell4.setCellValue(row.getCell(num).getStringCellValue());
-
+        return row.getCell(num).getStringCellValue();
     }
 
 
@@ -171,6 +187,16 @@ public class HandlerExcel {
         }
 
         return xssfWorkbook;
+    }
+
+
+    @Data
+    public static class ReturnMessage {
+
+
+        private BigDecimal bigDecimal;
+        private int count;
+
     }
 
 }
